3a39863203d9aba44656da14ce082043b77f07db
Asif047 <asifulhaque047@gmail.com>
2018-02-03 18:57:22 +0600

    first commit

---
+++ Rokomarians/app/src/main/java/com/cbc_app_poc/rokomari/rokomarians/HomeActivity.java
@@ -0,0 +1,164 @@
+package com.cbc_app_poc.rokomari.rokomarians;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout dl ;
+    private ActionBarDrawerToggle toggle ;
+    private Toolbar toolbar;
+
+    private TextView tvJourney,tvIdea,tvGoodWork,tvRecreation,tvWhat,tvHappyWall,tvMeetMe,
+                        tvFeeling;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        tvJourney=findViewById(R.id.textview_journey);
+        tvIdea=findViewById(R.id.textview_idea);
+        tvGoodWork=findViewById(R.id.textview_good_work);
+        tvRecreation=findViewById(R.id.textview_recreation);
+        tvWhat=findViewById(R.id.textview_what_should_i_do);
+        tvHappyWall=findViewById(R.id.textview_happy_wall);
+        tvMeetMe=findViewById(R.id.textview_meet_me);
+        tvFeeling=findViewById(R.id.textview_feeling);
+
+        Typeface tf=Typeface.createFromAsset(getAssets(), "font_amaranth/Amaranth-Bold.ttf");
+        tvJourney.setTypeface(tf);
+        tvIdea.setTypeface(tf);
+        tvGoodWork.setTypeface(tf);
+        tvRecreation.setTypeface(tf);
+        tvWhat.setTypeface(tf);
+        tvHappyWall.setTypeface(tf);
+        tvMeetMe.setTypeface(tf);
+        tvFeeling.setTypeface(tf);
+
+        getSupportActionBar().hide();
+
+
+        dl = (DrawerLayout) findViewById(R.id.dl);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        // Setting toolbar as the ActionBar with setSupportActionBar() call
+       // setSupportActionBar(toolbar);
+
+        toolbar.setTitle("Home Page");
+        dl.addDrawerListener(toggle);
+
+        toggle = new ActionBarDrawerToggle(this,dl,toolbar,R.string.open,R.string.close);
+
+        NavigationView nvDrawer = (NavigationView) findViewById(R.id.nv);
+        toggle.syncState();
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        setupDrawerContent(nvDrawer);
+
+
+        //new starts
+        View hView =  nvDrawer.getHeaderView(0);
+        TextView nav_user_name = (TextView)hView.findViewById(R.id.textview_name_header);
+        TextView nav_user_email = (TextView)hView.findViewById(R.id.textview_email_header);
+
+        nav_user_name.setText("Asif");
+        nav_user_email.setText("asif@rokomari.com");
+        //new ends
+    }
+
+
+
+    public void selectItemDrawer(MenuItem menuItem) {
+        Fragment myFragment = null;
+        Class fragmentClass;
+        switch (menuItem.getItemId()){
+            case R.id.home:
+//                Toast.makeText(RememberMeActivity.this,"hello",Toast.LENGTH_LONG).show();
+//                fragmentClass = Network.class;
+                break;
+            case R.id.profile:
+//                Intent intent1=new Intent(HomeActivity.this,AllNotesActivity.class);
+//                startActivity(intent1);
+                break;
+            case R.id.journey:
+//                Intent intent=new Intent(HomeActivity.this,MakeNoteActivity.class);
+//                startActivity(intent);
+                break;
+            case R.id.idea:
+//                Intent intent2=new Intent(HomeActivity.this,RememberMeActivity.class);
+//                startActivity(intent2);
+                break;
+            case R.id.good_work:
+//                Intent intent3=new Intent(HomeActivity.this,ShowWorkUpdateActivity.class);
+//                startActivity(intent3);
+                break;
+
+            case R.id.recreation:
+//                Intent intent3=new Intent(HomeActivity.this,ShowWorkUpdateActivity.class);
+//                startActivity(intent3);
+                break;
+
+            case R.id.what:
+//                Intent intent3=new Intent(HomeActivity.this,ShowWorkUpdateActivity.class);
+//                startActivity(intent3);
+                break;
+
+            case R.id.happy_wall:
+//                Intent intent3=new Intent(HomeActivity.this,ShowWorkUpdateActivity.class);
+//                startActivity(intent3);
+                break;
+
+            case R.id.meet_me:
+//                Intent intent3=new Intent(HomeActivity.this,ShowWorkUpdateActivity.class);
+//                startActivity(intent3);
+                break;
+
+            case R.id.feeling:
+//                Intent intent3=new Intent(HomeActivity.this,ShowWorkUpdateActivity.class);
+//                startActivity(intent3);
+                break;
+
+
+            default:
+                // fragmentClass = Network.class;
+        }
+        try {
+//            myFragment = (Fragment) fragmentClass.newInstance();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        fragmentManager.beginTransaction().replace(R.id.flcontent,myFragment).commit();
+        menuItem.setChecked(true);
+        //setTitle(menuItem.getTitle());
+        dl.closeDrawers();
+
+
+    }
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectItemDrawer(item);
+                return true;
+            }
+        });
+    }
+
+
+}
